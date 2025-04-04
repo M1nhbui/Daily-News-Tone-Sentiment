8e8dd99d871d73d00cf1bdd4a97497a107cda6f8
Minh Bui <buileminh342006@gmail.com>
2025-04-04 15:29:15 -0500

    Update

--- scripting/store_to_sql.py
+++ scripting/store_to_sql.py
@@ -1,13 +1,14 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_csv("data/processed_tones_articles.csv")
 
-engine = create_engine("sqlite:///data/news_sentiment.db")
+def save_to_sql():
+    df = pd.read_csv("data/processed_tones_articles.csv")
+    engine = create_engine("sqlite:///data/news_sentiment.db")
+    df.to_sql("news_sentiment", engine, if_exists="replace", index=False)
+    print("Data successfully stored in database!")
+    df1 = pd.read_sql("SELECT * FROM news_sentiment", engine)
+    print(df1)
 
-df.to_sql("news_sentiment", engine, if_exists="replace", index=False)
-
-print("Data successfully stored in database!")
-
-df1 = pd.read_sql("SELECT * FROM news_sentiment", engine)
-print(df1)
+if __name__ == "__main__":
+    save_to_sql()
